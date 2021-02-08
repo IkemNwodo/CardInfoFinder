fcdca37d70d81e51e3ea776311d460a420ba7871
Ikem <nwodofrank@gmail.com>
2021-02-08 09:24:35 +0100

    Network hilt module created

--- data/src/main/java/com/bankwithmit/data/di/DatabaseModule.kt
+++ data/src/main/java/com/bankwithmit/data/di/DatabaseModule.kt
@@ -3,21 +3,30 @@ package com.bankwithmit.data.di
 import android.app.Application
 import androidx.room.Room
 import com.bankwithmit.data.CardInfoDatabase
+import com.bankwithmit.data.localSource.dao.CardInfoDao
 import com.bankwithmit.data.utils.Constants.CARD_INFO_DATABASE
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class DatabaseModule {
 
+    @Singleton
     @Provides
     fun providesCardInfoDatabase(application: Application): CardInfoDatabase {
         return Room
             .databaseBuilder(application, CardInfoDatabase::class.java, CARD_INFO_DATABASE)
             .fallbackToDestructiveMigration()
             .build()
+    }
+
+    @Singleton
+    @Provides
+    fun providesCardInfoDao(cardInfoDatabase: CardInfoDatabase): CardInfoDao {
+        return cardInfoDatabase.cardInfoDao
     }
 }
--- data/src/main/java/com/bankwithmit/data/di/NetworkModule.kt
+++ data/src/main/java/com/bankwithmit/data/di/NetworkModule.kt
@@ -1,3 +1,51 @@
 package com.bankwithmit.data.di
 
-class NetworkModule
+import com.bankwithmit.data.remoteSource.api.CardInfoService
+import com.bankwithmit.data.utils.Constants.BASE_URL
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetworkModule{
+
+    @Singleton
+    @Provides
+    fun provideCardInfoService(retrofit: Retrofit): CardInfoService {
+        return retrofit.create(CardInfoService::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient, gson: Gson): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .client(okHttpClient)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpclient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideGson(): Gson {
+        val gsonBuilder = GsonBuilder()
+        return gsonBuilder.create()
+    }
+
+}
